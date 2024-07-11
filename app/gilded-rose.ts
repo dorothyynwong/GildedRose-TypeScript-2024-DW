851dced2a506ba262501b5c338587b5d5600126e
Dorothy Wong <dorothy.wong@softwire.com>
2024-07-11 15:09:49 +0100

    further cut backstage in switch

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -43,12 +43,12 @@ export class GildedRose {
           if (inBoundQuality(this.items[i].quality))
           {
             if (this.items[i].sellIn >= 0) {
-              if (this.items[i].sellIn < 6)
-                this.items[i].quality += 3
-              if (this.items[i].sellIn < 11 && this.items[i].sellIn >= 6)
-                this.items[i].quality += 2
               if (this.items[i].sellIn >= 11)
-                this.items[i].quality += 1
+                this.items[i].quality++;
+              else if (this.items[i].sellIn >=6) {
+                this.items[i].quality+=2;
+              } else this.items[i].quality+=3;
+              
             }
             else
               this.items[i].quality = 0
