1b86297ae759805469b8125e52d80154e789f7b0
Dorothy Wong <dorothy.wong@softwire.com>
2024-07-11 15:53:35 +0100

    move check in bound function

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,64 +17,56 @@ export class GildedRose {
     this.items = items;
   }
 
+isQualityInRange (itemQuality)  {
+    if (itemQuality > 0 && itemQuality < 50)
+      return true;
+    else
+      return false;
+  }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       const itemName = this.items[i].name;
-
-      const inBoundQuality = (itemQuality) => {
-        if (itemQuality>0 && itemQuality <50)
-          return true;
-        else
-          return false;
-      }
-
-      switch (itemName) {
+    
+      if (this.isQualityInRange(this.items[i].quality))
+      {switch (itemName) {
         case 'Aged Brie':
-          if (inBoundQuality(this.items[i].quality)) {
-            this.items[i].quality++
-          if (this.items[i].sellIn > 0)
-            this.items[i].sellIn--
-          else
-            this.items[i].quality++;
-
-          }
+            if (this.items[i].sellIn > 0)
+            {
+              this.items[i].quality++
+              this.items[i].sellIn--
+            }
+            else
+              this.items[i].quality+=2;
           break;
         case 'Backstage passes to a TAFKAL80ETC concert':
-          if (inBoundQuality(this.items[i].quality))
-          {
             if (this.items[i].sellIn >= 0) {
               if (this.items[i].sellIn >= 11)
                 this.items[i].quality++;
               else if (this.items[i].sellIn >=6) {
                 this.items[i].quality+=2;
               } else this.items[i].quality+=3;
-              
             }
             else
               this.items[i].quality = 0
-          }
           break;
         case 'Sulfuras, Hand of Ragnaros':
           break;
         case 'Conjured Mana Cake':
-          if (inBoundQuality(this.items[i].quality)) {
             if (this.items[i].sellIn > 0) {
               this.items[i].quality -= 2;
               this.items[i].sellIn--;
             }
             else this.items[i].quality-=4;
-          }
           break;
         default:
-          if (inBoundQuality(this.items[i].quality)) {
             if (this.items[i].sellIn > 0) {
               this.items[i].quality--;
               this.items[i].sellIn--;
             }
             else this.items[i].quality-=2;
-          }
           break;
-      }
+      }}
 /*
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
