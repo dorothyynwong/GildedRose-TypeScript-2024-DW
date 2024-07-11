29151ce18ed649d65388112ba53acaa675f4e69f
Dorothy Wong <dorothy.wong@softwire.com>
2024-07-11 15:01:01 +0100

    refractor switch case for backstage

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -23,28 +23,38 @@ export class GildedRose {
    //   let itemQuality = this.items[i].quality;
    //   let itemSellin = this.items[i].sellIn;
 
+      const inBoundQuality = (itemQuality) => {
+        if (itemQuality>0 && itemQuality <50)
+          return true;
+        else
+          return false;
+      }
+
       switch (itemName) {
         case 'Aged Brie':
-          if (this.items[i].quality > 0 && this.items[i].quality < 50)
+          if (inBoundQuality(this.items[i].quality)) {
             this.items[i].quality++
           if (this.items[i].sellIn > 0)
             this.items[i].sellIn--
-          if (this.items[i].sellIn < 0 && this.items[i].quality < 50)
-            this.items[i].quality++
+          else
+            this.items[i].quality++;
+
+          }
           break;
         case 'Backstage passes to a TAFKAL80ETC concert':
-          if (this.items[i].quality > 0 && this.items[i].quality < 50)
-            this.items[i].quality++
-          if (this.items[i].quality > 0 && this.items[i].quality < 50 && this.items[i].sellIn < 11)
-            this.items[i].quality += 1
-          if (this.items[i].quality > 0 && this.items[i].quality < 50 && this.items[i].sellIn < 6)
-            this.items[i].quality += 1
-          if (this.items[i].quality > 0 && this.items[i].quality < 50 && this.items[i].sellIn <= 0)
-            this.items[i].quality += 1
-          if (this.items[i].sellIn > 0)
-            this.items[i].sellIn--
-          if (this.items[i].sellIn < 0)
-            this.items[i].quality = 0
+          if (inBoundQuality(this.items[i].quality))
+          {
+            if (this.items[i].sellIn >= 0) {
+              if (this.items[i].sellIn < 6)
+                this.items[i].quality += 3
+              if (this.items[i].sellIn < 11 && this.items[i].sellIn >= 6)
+                this.items[i].quality += 2
+              if (this.items[i].sellIn >= 11)
+                this.items[i].quality += 1
+            }
+            else
+              this.items[i].quality = 0
+          }
           break;
         case 'Sulfuras, Hand of Ragnaros':
           break;
