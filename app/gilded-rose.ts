8cc60541cc3655f6682f49daaa7ef75c745e768a
Dorothy Wong <dorothy.wong@softwire.com>
2024-07-11 14:26:38 +0100

    continue to move code to switch case

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -27,16 +27,28 @@ export class GildedRose {
         case 'Aged Brie':
           if (this.items[i].quality > 0 && this.items[i].quality < 50)
             this.items[i].quality++
+          if (this.items[i].sellIn > 0)
+            this.items[i].sellIn--
           break;
         case 'Backstage passes to a TAFKAL80ETC concert':
           if (this.items[i].quality > 0 && this.items[i].quality < 50)
             this.items[i].quality++
+          if (this.items[i].quality > 0 && this.items[i].quality < 50 && this.items[i].sellIn < 11)
+            this.items[i].quality += 1
+          if (this.items[i].quality > 0 && this.items[i].quality < 50 && this.items[i].sellIn < 6)
+            this.items[i].quality += 1
+          if (this.items[i].quality > 0 && this.items[i].quality < 50 && this.items[i].sellIn <= 0)
+            this.items[i].quality += 1
+          if (this.items[i].sellIn > 0)
+            this.items[i].sellIn--
           break;
         case 'Sulfuras, Hand of Ragnaros':
           break;
         default:
           if (this.items[i].quality > 0)
             this.items[i].quality--;
+          if (this.items[i].sellIn > 0)
+            this.items[i].sellIn--
           break;
       }
 
@@ -52,19 +64,19 @@ export class GildedRose {
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                // this.items[i].quality = this.items[i].quality + 1
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                // this.items[i].quality = this.items[i].quality + 1
               }
             }
           }
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        // this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
